3bc3b5bed1c3c64a0a41a5c57133158dd284aa76
sirdanke <ahmad.syukron6991@gmail.com>
2019-03-15 11:16:40 +0700

    clean the login

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 let app = new Vue({
     el : `#app`,
     data : {
-        
+        isLogin : false
+    },
+    methods : {
+        successLogin() {
+            this.isLogin = true
+        }
     }
 })
